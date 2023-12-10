11bf809b10ffee92ad3f6beb3a231214a7b162da
Ali Kemal Ofluoglu <alikemof@gmail.com>
2023-12-10 20:01:34 +0300

    Update LinksData.js

--- data/LinksData.js
+++ data/LinksData.js
@@ -3,86 +3,58 @@ const webLinks = [
     // All social profile
     {
         title: 'Twitter',
-        url: 'https://twitter.com/realvjy',
+        url: 'https://twitter.com/souljahn2',
         type: 'social',
         icon: '/twitter.svg',
         on: true
     },
     {
         title: 'Instagram',
-        url: 'https://instagram.com/realvjy',
+        url: 'https://instagram.com/souljahn2',
         type: 'social',
         icon: '/insta.svg',
         on: true
     },
     {
-        title: 'Dribbble',
-        url: 'https://dribbble.com/realvjy',
-        type: 'social',
-        icon: '/dribbble.svg',
-        on: false
-    },
-    {
-        title: 'Medium',
-        url: 'https://medium.com/@realvjy',
-        type: 'social',
-        icon: '/medium.svg',
-        on: false
-    },
-    {
         title: 'Github',
-        url: 'https://github.com/realvjy',
+        url: 'https://github.com/souljahn2',
         type: 'social',
         icon: '/github.svg',
         on: true
     },
     {
         title: 'Youtube',
-        url: 'https://youtube.com/realvjy',
+        url: 'https://www.youtube.com/@souljahn2',
         type: 'social',
         icon: '/youtube.svg',
         on: false
     },
     {
-        title: 'Figma',
-        url: 'https://figma.com/@realvjy',
-        type: 'social',
-        icon: '/figma.svg',
-        on: true
-    },
-    {
         title: 'Behance',
-        url: 'https://behance.net/realvjy',
+        url: 'https://www.behance.net/souljahn2',
         type: 'social',
         icon: '/behance.svg',
         on: false
     },
     {
         title: 'Linkedin',
-        url: 'https://www.linkedin.com/in/realvjy/',
+        url: 'https://www.linkedin.com/in/alikemal/',
         type: 'social',
         icon: '/linkedin.svg',
         on: false
     },
     {
         title: 'Mastodon',
-        url: 'https://mastodon.online/@realvjy',
+        url: 'https://mastodon.online/@souljahn2',
         type: 'social',
         icon: '/mastodon.svg',
         on: true
     },
     {
         title: 'Hive Social',
-        url: 'https://www.hivesocial.app/?@realvjy',
+        url: 'https://www.hivesocial.app/?@souljahn2',
         type: 'social',
         icon: '/hive.svg',
-        on: false
-    },
-    {
-        title: 'Post',
-        url: 'https://www.post.news/realvjy',
-        type: 'social',
-        icon: '/post.svg',
         on: false
     },
     {
