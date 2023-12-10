c7f9e6d53eb779d422063abc6158132ca755cb51
Ali Kemal Ofluoglu <alikemof@gmail.com>
2023-12-10 18:14:44 +0300

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,21 +1,21 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
+        name: 'souljahn2 web',
+        username: 'souljahn2web.vercel.app',
+        url: 'https://souljahn2web.vercel.app',
         titleImg: true,
         avatar: '/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'SoulJahN2 Bio Links ',
         descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
+        subdesc: 'Vercelde ücretsiz olarak host edilmektedir.',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
         newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
+        author: 'souljahn2',
+        authorURL: 'https://github.com/souljahn2',
+        footerText: 'yapanzi'
     },
 ];
 
